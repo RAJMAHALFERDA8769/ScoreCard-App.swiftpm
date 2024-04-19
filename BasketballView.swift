420248188805d69044c659f3451db86844fee1a1
MylesMosier <mmosier5246@stu.214.org>
2024-04-19 13:03:43 -0500

    Made SportSelectView

---
+++ BasketballView.swift
@@ -0,0 +1,19 @@
+//
+//  BasketballView.swift
+//  ScoreCard App
+//
+//  Created by Myles Mosier on 4/19/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct BasketballView: View {
+    var body: some View {
+        VStack {
+            Text("HI")
+            }
+        }
+    }
+
+
---
+++ SportSelectView.swift
@@ -0,0 +1,20 @@
+//
+//  SportSelectView.swift
+//  ScoreCard App
+//
+//  Created by Myles Mosier on 4/19/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct SportSelectView: View {
+    var body: some View {
+        VStack {
+            Button("Basketball") {
+                BasketballView()
+            }
+        }
+    }
+}
+
