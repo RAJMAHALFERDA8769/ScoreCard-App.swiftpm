5a5210d3441ad91d634fd350bc9b86de007933e0
Mateus Salomao <msalomao6361@stu.d214.org>
2024-05-01 12:58:47 -0500

    Remade TableView Corrctly

--- TableView.swift
+++ TableView.swift
@@ -1,23 +1,9 @@
 import Foundation
 import SwiftUI
 
-struct PlayerStats: Identifiable {
-    var id = UUID()
-    var playerName: String
-    var points: Int
-    var assists: Int
-    var rebounds: Int
-    var blocks: Int
-    var steals: Int
-    var fouls: Int
-}
-struct TableView: View {
-    @State var playersStats: [PlayerStats] = [
-        PlayerStats(playerName: "Player 1", points: 0, assists: 0, rebounds: 0, blocks: 0, steals: 0, fouls: 0),
-        PlayerStats(playerName: "Player 2", points: 0, assists: 0, rebounds: 0, blocks: 0, steals: 0, fouls: 0),
-        PlayerStats(playerName: "Player 3", points: 0, assists: 0, rebounds: 0, blocks: 0, steals: 0, fouls: 0),
-        PlayerStats(playerName: "Player 4", points: 0, assists: 0, rebounds: 0, blocks: 0, steals: 0, fouls: 0),
-        PlayerStats(playerName: "Player 5", points: 0, assists: 0, rebounds: 0, blocks: 0, steals: 0, fouls: 0)
-    ]
-}
 
+struct TableView: View {
+    var body: some View {
+        Text("Hi")
+    }
+}
