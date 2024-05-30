e1d430f8f42ceca0ea1c04ba590deb13412bf85e
ArunRajeishbabu <arajeishbabu5302@stu.d214.org>
2024-05-30 12:41:32 -0500

    fixed grammer

--- RajView.swift
+++ RajView.swift
@@ -11,6 +11,6 @@ import SwiftUI
 struct RajView: View {
     var body: some View {
         Image("Raj")
-        Text("Hi Im RJ and I'm the best golf playerof the group")
+        Text("Hi Im RJ and I'm the best golf player of the group")
     }
 }
