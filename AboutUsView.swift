80912c03b1b477e741a1476750d467178b4a4e83
ArunRajeishbabu <arajeishbabu5302@stu.d214.org>
2024-05-30 12:08:57 -0500

    made about us view

--- AboutUsView.swift
+++ AboutUsView.swift
@@ -11,13 +11,41 @@ import SwiftUI
 struct AboutUsView: View {
     var body: some View {
         VStack {
+            Text("The Creators")
+                .font(.largeTitle)
+                .bold()
+            Spacer()
             Text("Raj")
-                .font(.title)
+                .font(.largeTitle)
                 .foregroundColor(.white)
                 .padding()
                 .background(Color.blue)
                 .cornerRadius(10)
                 .padding(.bottom, 20)
+            Text("Myles")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(10)
+                .padding(.bottom, 20)
+            Text("Adam")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(10)
+                .padding(.bottom, 20)
+            Text("Mateus")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(10)
+                .padding(.bottom, 20)
+            
+            
+            
             
         }
     }
